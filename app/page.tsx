486468395701a80fc3d364d2199c9050b7c3d875
Ian <iandavis1504@gmail.com>
2025-07-15 16:33:38 -0400

    chore: fixed linter error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <h1 className="text-3xl font-bold">
-                DES BOMS - it's alive!
+                DES BOMS - its alive!
             </h1>
         </main>
     );
